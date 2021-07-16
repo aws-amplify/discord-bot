0d33a165d919c664af19c49545a36a4f4eddc809
josef <josef.aidt@gmail.com>
2021-07-16 09:30:02 -0500

    add static commands to bank

--- src/commands/_bank.js
+++ src/commands/_bank.js
@@ -1,6 +1,20 @@
+import { readFileSync } from 'fs'
 import * as hello from './hello.js'
 
-const commands = [hello]
+const staticCommandsData = JSON.parse(
+  readFileSync(new URL('./static-commands.json', import.meta.url), 'utf-8')
+)
+const staticCommands = Object.entries(staticCommandsData).map(
+  ([staticCommandName, staticCommandReturnValue]) => {
+    return {
+      config: {
+        name: staticCommandName,
+      },
+      handler: () => staticCommandReturnValue,
+    }
+  }
+)
+const commands = [hello, ...staticCommands]
 
 export const bank = new Map(
   commands.map((command) => [command.config.name, command])
