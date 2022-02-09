c5b3c2c10a02f4bc878ae993992485f9a6237cc1
josefaidt <josef.aidt@gmail.com>
2022-02-09 08:45:52 -0600

    feat(discord): add api wrapper, improve registration handling

--- packages/discord/src/discord/registerCommand.js
+++ packages/discord/src/discord/registerCommand.js
@@ -1,44 +1,13 @@
-import fetch from 'node-fetch'
+import { createAPI } from './api.js'
 
-export async function registerCommand(command, { guildId }) {
-  const config = {
-    method: 'POST',
-    headers: {
-      Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(command.config),
-  }
-  let url = `https://discord.com/api/v8/applications/${process.env.DISCORD_APP_ID}/commands`
+export async function registerCommandV(command, { guildId } = {}) {
+  const api = createAPI(process.env.DISCORD_BOT_TOKEN)
+
+  let url = `/applications/${process.env.DISCORD_APP_ID}`
   if (guildId) {
-    url = `https://discord.com/api/v8/applications/${process.env.DISCORD_APP_ID}/guilds/${guildId}/commands`
+    url += `/guilds/${guildId}`
   }
+  url += '/commands'
 
-  let data
-  try {
-    console.log(`Registering ${command.config.name}`)
-    const response = await fetch(url, config)
-    if (response.ok && (response.status === 200 || response.status === 201)) {
-      console.log(
-        `${response.status === 201 ? 'Created' : 'Updated'} ${
-          command.config.name
-        } successfully`
-      )
-      data = await response.json()
-    } else {
-      data.errors = [
-        {
-          message: `Unable to register ${command.config.name}`,
-          status: response.status,
-        },
-      ]
-    }
-  } catch (error) {
-    throw new Error(
-      `Error registering command ${command?.config?.name}:`,
-      error
-    )
-  }
-
-  return data
+  return api.post(url, command.config)
 }
--- packages/discord/src/discord/syncCommands.js
+++ packages/discord/src/discord/syncCommands.js
@@ -1,7 +1,21 @@
 import { bank } from '@hey-amplify/bank'
-import { registerCommand } from './registerCommand.js'
+import { registerCommandV2 } from './registerCommand.js'
 
 export async function syncCommands() {
-  const commands = Array.from(bank.values()).map(registerCommand)
-  return await Promise.allSettled(commands)
+  const commands = Array.from(bank.values()).map(registerCommandV2)
+  let result = []
+  for await (let registered of commands) {
+    if (registered.error) {
+      console.error(
+        `Error registering command ${registered.error.name}:`,
+        registered.error
+      )
+    } else {
+      result.push(registered.data)
+    }
+  }
+  if (!result.length) {
+    throw new Error('No commands registered')
+  }
+  return result
 }
