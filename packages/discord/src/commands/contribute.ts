fb2c2212cd0af8aa030e4aaf1a98404744456e4b
josefaidt <josef.aidt@gmail.com>
2022-04-02 10:18:09 -0500

    feat: add repository contribution links

--- packages/discord/src/commands/contribute.ts
+++ packages/discord/src/commands/contribute.ts
@@ -8,24 +8,38 @@ const repository = createOption({
   choices: ['cli'],
 })
 
+function getRepositoryContributeUrl(repository) {
+  switch (repository) {
+    case 'android':
+      return 'https://github.com/aws-amplify/amplify-android/contribute'
+    case 'cli':
+      return 'https://github.com/aws-amplify/amplify-cli/contribute'
+    case 'docs':
+      return 'https://github.com/aws-amplify/docs/contribute'
+    case 'flutter':
+      return 'https://github.com/aws-amplify/amplify-flutter/contribute'
+    case 'ios':
+      return 'https://github.com/aws-amplify/amplify-ios/contribute'
+    case 'js':
+      return 'https://github.com/aws-amplify/amplify-js/contribute'
+    case 'ui':
+      return 'https://github.com/aws-amplify/amplify-ui/contribute'
+    default:
+      return `🤢 something went wrong loading the contribute URL for the ${repository} repository`
+  }
+}
+
 export default createCommand({
   name: 'contribute',
   description: 'Learn how to contribute to an Amplify project',
   options: [repository],
   handler: (context) => {
     const [repository] = context.data.options
-    switch (repository) {
-      case 'cli': {
-        const lines = [
-          'Thanks for your interest in contributing!',
-          'Contributing guide: https://github.com/aws-amplify/amplify-cli/blob/master/CONTRIBUTING.md',
-          'Good first issues: https://github.com/aws-amplify/amplify-cli/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22',
-        ]
-        const message = lines.join('\n')
-        return message
-      }
-      default:
-        return '🤢 something went wrong'
-    }
+    const lines = [
+      'Thanks for your interest in contributing! To learn how to get started visit the contributing guide:',
+      getRepositoryContributeUrl(repository),
+    ]
+    const message = lines.join('\n')
+    return message
   },
 })
