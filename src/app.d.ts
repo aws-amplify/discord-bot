3e5da8162b51dbd4750990ef62128710eb7fea7b
josefaidt <josef.aidt@gmail.com>
2022-07-13 08:21:22 -0500

    hotfix: next-auth session typing

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,12 +4,14 @@ import type * as NextAuth from 'next-auth'
 
 // See https://kit.svelte.dev/docs/typescript
 // for information about these interfaces
-declare namespace App {
-  interface Locals {}
+declare global {
+  namespace App {
+    interface Locals {}
 
-  interface Platform {}
+    interface Platform {}
 
-  interface Session extends NextAuth.Session {}
+    interface Session extends NextAuth.Session {}
 
-  interface Stuff {}
+    interface Stuff {}
+  }
 }
