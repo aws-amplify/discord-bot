34cdc0c11a5fad900303d9d8d2cc6439f4abd3c5
josefaidt <josef.aidt@gmail.com>
2022-07-12 21:44:31 -0500

    chore: add error page for auth

---
+++ src/routes/auth/error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+  /** @type {import('@sveltejs/kit').Load} */
+  export function load({ url }) {
+    const { searchParams } = new URL(url)
+    return {
+      props: {
+        title: `${searchParams}`,
+      },
+    }
+  }
+</script>
+
+<script lang="ts">
+  import { Content } from 'carbon-components-svelte'
+
+  export let title
+</script>
+
+<Content>
+  <h1>{title}</h1>
+</Content>
