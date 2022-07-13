2d4d337d7615096f87e6355bfc8abf2dd670b96e
josefaidt <josef.aidt@gmail.com>
2022-07-13 08:30:44 -0500

    feat: add cds style override file, layout to use app session

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,42 +1,29 @@
-<script context="module">
-  /** * @type {import('@sveltejs/kit').Load} */
-  export async function load({ session }) {
-    return {
-      props: { user: session?.user || false },
-    }
-  }
-</script>
-
 <script lang="ts">
   import {
     Header,
     SkipToContent,
     Theme,
-    Button,
     HeaderUtilities,
-    HeaderGlobalAction,
     HeaderAction,
     HeaderPanelLinks,
     HeaderPanelLink,
     ToastNotification,
   } from 'carbon-components-svelte'
   import 'carbon-components-svelte/css/all.css'
+  import { session } from '$app/stores'
   import { afterNavigate } from '$app/navigation'
   import Avatar from '$lib/Avatar.svelte'
   import LoginButton from '$lib/LoginButton.svelte'
-  import LogoutButton from '$lib/LogoutButton.svelte'
   import { user as userStore, notifications } from '$lib/store'
   import type { CarbonTheme } from 'carbon-components-svelte/types/Theme/Theme.svelte'
-
-  export let user
-  userStore.set(user)
+  import '../app.css'
 
   let theme: CarbonTheme = 'g100'
 
   let isSideNavOpen = false
   let isUserPanelOpen = false
 
-  afterNavigate(navigation => {
+  afterNavigate((navigation) => {
     if (isUserPanelOpen) isUserPanelOpen = false
   })
 </script>
@@ -54,13 +41,13 @@
     </span>
 
     <HeaderUtilities>
-      {#if $userStore}
+      {#if $session}
         <HeaderAction
           aria-label="User settings"
           bind:isOpen="{isUserPanelOpen}"
           transition="{{ duration: 200 }}"
         >
-          <Avatar slot="icon" url="{user.image}" />
+          <Avatar slot="icon" url="{$session.user.image}" />
           <HeaderPanelLinks>
             {#if $userStore.isAdmin}
               <HeaderPanelLink href="/admin">Administration</HeaderPanelLink>
@@ -84,25 +71,12 @@
 </Theme>
 
 <style>
-  :global(body) {
-    /* --cds-interactive-01: #5865f2;
-    --cds-hover-primary: #7289da; */
-  }
-
-  :global(.bx--content) {
-    background: inherit;
-  }
-
   :global(.bx--col > h1) {
     font-size: var(--cds-display-01-font-size);
     font-weight: var(--cds-display-01-font-weight);
     letter-spacing: var(--cds-display-01-letter-spacing);
     line-height: var(--cds-display-01-line-height);
     margin-bottom: var(--cds-layout-01);
-  }
-
-  :global(.bx--content) {
-    background-color: var(--cds-ui-background);
   }
 
   div.ha--notification--container {
