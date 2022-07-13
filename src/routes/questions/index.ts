ef830d4d2bd09e1260ea690fd4ebe891cc619ec1
josefaidt <josef.aidt@gmail.com>
2022-07-13 16:12:35 -0500

    hotfix: questions should always return 2xx, fixes fuzzy dep

--- src/routes/questions/index.ts
+++ src/routes/questions/index.ts
@@ -3,14 +3,8 @@ import { prisma } from '$lib/db'
 export async function get() {
   const questions = await prisma.question.findMany()
 
-  if (questions?.length) {
-    return {
-      status: 200,
-      body: { questions },
-    }
-  }
-
   return {
-    status: 500,
+    status: questions?.length ? 200 : 204,
+    body: { questions },
   }
 }
