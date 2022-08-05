27e8482905bd2684acc032880962925f8467c4ee
Emma Sauerborn <esauerbo@amazon.com>
2022-08-05 16:52:56 -0700

    adding discussion to db

--- src/lib/discord/commands/admin.ts
+++ src/lib/discord/commands/admin.ts
@@ -21,19 +21,19 @@ import type {
 
 const userIdToUsername = new Map<string, user>()
 
-// const iconMap = new Map<string, string>([
-//   ['Admin', "https://github.com/esauerbo1/Images/blob/main/Admin.svg"],
-//   ['Bot', 'https://github.com/esauerbo1/Images/blob/main/Bot.svg'],
-//   ['Amplify Bot Dev', 'https://github.com/esauerbo1/Images/blob/main/Amplify-Bot-Dev.svg'],
-//   ['Moderator', 'https://github.com/esauerbo1/Images/blob/main/Moderator.svg'],
-//   ['Amplify Staff', 'https://github.com/esauerbo1/Images/blob/main/Amplify-Staff.svg'],
-//   ['AWS Staff', 'https://github.com/esauerbo1/Images/blob/main/AWS-Staff.svg'],
-//   ['Community Builder', 'https://github.com/esauerbo1/Images/blob/main/Community-Builder.svg'],
-//   ['Contributor', 'https://github.com/esauerbo1/Images/blob/main/Contributor.svg'],
-//   ['Meetup Organizer', 'https://github.com/esauerbo1/Images/blob/main/Meetup-Organizer.svg'],
-//   ['Amplify Guru', 'https://github.com/esauerbo1/Images/blob/main/Amplify-Guru.svg'],
-//   ['@everyone', 'https://github.com/esauerbo1/Images/blob/main/%40everyone.svg'],
-// ])
+const iconMap = new Map<string, string>([
+  ['Admin', '#FD0061'],
+  ['Bot', '#7A2F8F'],
+  ['Amplify Bot Dev', '#969C9F'],
+  ['Moderator', '#0099E1'],
+  ['Amplify Staff', '#CC7900'],
+  ['AWS Staff', '#F93A2F'],
+  ['Community Builder', '#00D166'],
+  ['Contributor', '#00C09A'],
+  ['Meetup Organizer', '#F8C300'],
+  ['Amplify Guru', '#4E6F7B'],
+  ['at-everyone', '#91A6A6'],
+])
 
 type user = {
   username: string
@@ -43,8 +43,10 @@ type user = {
 
 function getUser(userId: string, guildMember: GuildMember | null) {
   if (!userIdToUsername.get(userId)) {
-    let role = '@everyone'
+    let img
+    let role = 'at-everyone'
     if (guildMember?.roles?.highest?.name) role = guildMember.roles.highest.name
+
     userIdToUsername.set(`${userId}`, {
       username: `${faker.unique(faker.color.human)} ${faker.unique(
         faker.hacker.noun
@@ -99,6 +101,26 @@ export const config = new SlashCommandBuilder()
       )
   )
 
+async function addDiscussion(discussion, userId: string, record) {
+  const githubDiscussion = {
+    id: discussion?.createDiscussion?.discussion?.id,
+    url: discussion?.createDiscussion?.discussion?.url,
+    createdBy: userId,
+    Question: {
+      connect: {
+        id: record.id,
+      },
+    },
+  }
+  try {
+    await prisma.gitHubDiscussion.create({
+      data: githubDiscussion,
+    })
+  } catch (error) {
+    console.error(`Failed to add discussion to db ${error.message}`)
+  }
+}
+
 export async function handler(
   interaction: ChatInputCommandInteraction
 ): Promise<InteractionReplyOptions | string> {
@@ -146,7 +168,14 @@ export async function handler(
   const title = record?.title
   const body = createDiscussionBody(firstMessage, messages, record?.url)
   let answerContent
-
+  try {
+    const img = await fetch(
+      `${process.env.VITE_NEXTAUTH_URL}/api/p/colors/${'#FD0061'}`
+    )
+    console.log(img)
+  } catch (error) {
+    console.error('failed to fetch image')
+  }
   // if the answer is solved create solution content
   if (record.isSolved) {
     const answerRecord = await prisma.answer.findUnique({
@@ -174,6 +203,8 @@ export async function handler(
         body,
         mutationId: record.id,
       })
+      if (postResponse)
+        addDiscussion(postResponse, interaction?.user?.id, record)
       if (answerContent) {
         try {
           const answerResponse = await postAnswer({
@@ -191,13 +222,10 @@ export async function handler(
       }
       await lockDiscussion({
         discussionId: postResponse?.createDiscussion?.discussion?.id,
-        clientMutationId: record.id
+        clientMutationId: record.id,
       })
       if (postResponse?.createDiscussion?.discussion?.url) {
-      //  interaction.editReply({content: `📦 ${postResponse.createDiscussion.discussion.url}`})
-      console.log("a")
-        return `📦 ${postResponse.createDiscussion.discussion.url}`
-        
+        return `📦 ${postResponse?.createDiscussion?.discussion?.url}`
       }
     } catch (error) {
       return somethingWentWrongResponse(': failed to post discussion')
---
+++ src/routes/api/p/colors/[code].ts
@@ -0,0 +1,28 @@
+import type { RequestHandler } from '@sveltejs/kit'
+
+const svg = (color: string) => `
+  <svg 
+    fill="${color}"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink= "http://www.w3.org/1999/xlink"
+  >
+    <circle cx="50" cy="50" r="40" />
+  </svg>
+`
+
+export const GET: RequestHandler = ({ params }) => {
+  const { code } = params
+  const color = `#${code.replace(/\.svg$/, '')}`
+  return {
+    headers: {
+      // store for 365 days
+      'cache-control': import.meta.env.PROD
+        ? 'public, max-age=31536000, immutable'
+        : 'no-cache',
+      // store for 30 days
+      // 'cache-control': 'public, max-age=2592000, immutable',
+      'content-type': 'image/svg+xml',
+    },
+    body: svg(color),
+  }
+}
