429edb427f452c20d839128174f791aa7f4cf6ba
josef <josef.aidt@gmail.com>
2022-08-10 14:29:56 -0500

    fix(test): remove placeholder unit tests from commands (#222)

--- src/lib/discord/commands/login.ts
+++ src/lib/discord/commands/login.ts
@@ -1,22 +1,23 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
-import type { ChatInputCommandInteraction, InteractionReplyOptions } from 'discord.js'
+import type {
+  ChatInputCommandInteraction,
+  InteractionReplyOptions,
+} from 'discord.js'
 
 export const config = new SlashCommandBuilder()
   .setName('login')
   .setDescription('Login to GitHub to link your accounts.')
 
-export function handler(interaction: ChatInputCommandInteraction): InteractionReplyOptions | string {
+export function handler(
+  interaction: ChatInputCommandInteraction
+): InteractionReplyOptions | string {
   const { user } = interaction
 
   if (user.bot) {
     return 'This command does not support bots logging in.'
-  } else return {
-    content: `${import.meta.env.VITE_HOST}/profile/link`,
-    ephemeral: true,
-  }
-}
-
-if (import.meta.vitest) {
-  const { test } = import.meta.vitest
-  test.todo('/login')
+  } else
+    return {
+      content: `${import.meta.env.VITE_HOST}/profile/link`,
+      ephemeral: true,
+    }
 }
