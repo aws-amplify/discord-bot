74660f02cbe030f63568ed4da45fd64afe75b70a
josefaidt <josef.aidt@gmail.com>
2022-08-12 15:45:30 -0500

    fix(app): constants import

--- src/lib/github/apply-roles.ts
+++ src/lib/github/apply-roles.ts
@@ -2,7 +2,7 @@ import { createAppAuth } from '@octokit/auth-app'
 import { Octokit } from '@octokit/rest'
 import { addRole } from '$discord/roles/addRole'
 import { removeRole } from '$discord/roles/removeRole'
-import { ACCESS_LEVELS } from '/constants'
+import { ACCESS_LEVELS } from '$lib/constants'
 import { prisma } from '$lib/db'
 
 /**
