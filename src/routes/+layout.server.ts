9822b9f2cc681c2dc805a31280afbebc20c2fd37
josefaidt <josef.aidt@gmail.com>
2022-09-05 15:55:42 -0500

    feat(app): add multi-guild support

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,11 +1,32 @@
-import type { LayoutServerLoad } from './$types'
+import { api } from '$discord/api'
+import { Routes, type APIGuild } from 'discord-api-types/v10'
+import { type LayoutServerLoad } from './$types'
 
-export const getSession = (event): Promise<App.Session> => {
-  return event.locals.session
-}
+export const load: LayoutServerLoad = async ({ locals }) => {
+  const botGuilds = (await api.get(Routes.userGuilds())) as APIGuild[]
 
-export const load: LayoutServerLoad = ({ locals }) => {
+  const guilds = []
+  for (const guild of botGuilds) {
+    try {
+      await api.get(Routes.guildMember(guild.id, locals.session.user.id))
+      guilds.push(guild)
+    } catch (error) {
+      // user is not a member of this guild
+    }
+  }
+
+  const defaultGuildId = import.meta.env.VITE_DISCORD_GUILD_ID
+
   return {
     session: locals.session,
+    guilds: guilds.map((guild) => ({
+      id: guild.id,
+      text: guild.name,
+      icon: guild.icon
+        ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`
+        : null,
+    })),
+    selectedGuild:
+      guilds.find((g) => g.id === locals?.session?.guild)?.id || defaultGuildId,
   }
 }
