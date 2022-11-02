9ae40b9a5c60d423f842515f6789c49555aea316
josefaidt <josef.aidt@gmail.com>
2022-11-02 11:28:49 -0700

    chore(app): only admins can access admin page

--- src/routes/admin/+layout.server.ts
+++ src/routes/admin/+layout.server.ts
@@ -3,11 +3,7 @@ import type { LayoutServerLoad } from './$types'
 
 export const load: LayoutServerLoad = async ({ parent }) => {
   const { session } = await parent()
-  if (
-    session?.user?.isAdmin ||
-    session?.user?.isStaff ||
-    session?.user?.isGuildOwner
-  ) {
+  if (session?.user?.isAdmin || session?.user?.isGuildOwner) {
     return {}
   }
   throw redirect(302, '/restricted')
