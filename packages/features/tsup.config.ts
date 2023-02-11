263ae356d7d3195f086576e14888b6f0199c03c4
josefaidt <josef.aidt@gmail.com>
2023-02-11 13:41:23 -0800

    rm unecessary external value

--- packages/features/tsup.config.ts
+++ packages/features/tsup.config.ts
@@ -7,9 +7,8 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
   outDir: 'build',
-  external: ['./src/generated/client/index.js'],
   dts: true,
   sourcemap: true,
   clean: true,
-  target: 'node',
+  target: 'node18',
 })
