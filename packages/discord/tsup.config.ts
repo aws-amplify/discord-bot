ab82bd2e35d98697764e3821f2f957caf3ff2ea3
josefaidt <josef.aidt@gmail.com>
2023-02-13 10:39:17 -0800

    fix dynamic require issue where pkgs are importing cjs

--- packages/discord/tsup.config.ts
+++ packages/discord/tsup.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-  define: {
-    'import.meta.vitest': 'undefined',
-  },
-  entry: ['src/index.ts'],
-  format: ['esm'],
-  outDir: 'build',
-  dts: true,
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  target: 'node18',
+export default defineConfig((options) => {
+  return {
+    define: {
+      'import.meta.vitest': 'undefined',
+    },
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    outDir: 'build',
+    dts: true,
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    // https://github.com/evanw/esbuild/issues/946#issuecomment-814703190
+    banner: {
+      js: "import { createRequire as topLevelCreateRequire } from 'module';\n const require = topLevelCreateRequire(import.meta.url);",
+    },
+    target: 'node18',
+  }
 })
