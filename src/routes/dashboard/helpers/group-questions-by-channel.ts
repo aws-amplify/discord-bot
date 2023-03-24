1e0a71b468077f87a72de03ebe7b0deb93cb9b84
josefaidt <josef.aidt@gmail.com>
2023-03-24 15:36:56 -0700

    fix reduce return type, add group by cohort

--- src/routes/dashboard/helpers/group-questions-by-channel.ts
+++ src/routes/dashboard/helpers/group-questions-by-channel.ts
@@ -4,14 +4,17 @@ import type { Question } from '../types'
  * Group questions by channel
  */
 export function groupQuestionsByChannel(questions: Question[]) {
-  const grouped = questions.reduce((acc, question) => {
-    if (acc[question.channelName]) {
-      acc[question.channelName].push(question)
-    } else {
-      acc[question.channelName] = [question]
-    }
-    return acc
-  }, {} as Record<string, Question[]>)
+  const grouped = questions.reduce<Record<string, Question[]>>(
+    (acc, question) => {
+      if (acc[question.channelName]) {
+        acc[question.channelName].push(question)
+      } else {
+        acc[question.channelName] = [question]
+      }
+      return acc
+    },
+    {}
+  )
 
   return grouped
 }
--- src/routes/dashboard/helpers/group-questions-by-tag.ts
+++ src/routes/dashboard/helpers/group-questions-by-tag.ts
@@ -4,18 +4,21 @@ import type { Question } from '../types'
  * Group questions by tag
  */
 export function groupQuestionsByTag(questions: Question[]) {
-  const grouped = questions.reduce((acc, question) => {
-    if (question.tags) {
-      for (const tag of question.tags) {
-        if (acc[tag.name]) {
-          acc[tag.name].push(question)
-        } else {
-          acc[tag.name] = [question]
+  const grouped = questions.reduce<Record<string, Question[]>>(
+    (acc, question) => {
+      if (question.tags) {
+        for (const tag of question.tags) {
+          if (acc[tag.name]) {
+            acc[tag.name].push(question)
+          } else {
+            acc[tag.name] = [question]
+          }
         }
       }
-    }
-    return acc
-  }, {} as Record<string, Question[]>)
+      return acc
+    },
+    {}
+  )
 
   return grouped
 }
