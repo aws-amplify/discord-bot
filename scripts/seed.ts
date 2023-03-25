06e3d9b946317b8aab6af805a02a5e991ef64883
josefaidt <josef.aidt@gmail.com>
2023-03-25 11:10:04 -0700

    improve seed script, creates 3000 questions

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker'
+import task from 'tasuku'
 import { prisma, init } from '$lib/db'
 import { ACCESS_LEVELS } from '$lib/constants'
 import type { DiscordUser, DiscordRole, Prisma } from '@prisma/client'
@@ -18,12 +19,10 @@ async function getRandomFakeUser(): Promise<DiscordUser> {
 }
 
 function createFakeRoles(): Prisma.DiscordRoleCreateInput[] {
-  const access = Object.values(ACCESS_LEVELS)
-  return Array.from({ length: 10 }).map((_, i) => {
-    const id = faker.random.numeric(18)
-    const name = access[Math.floor(Math.random() * access.length)]
-    return {
-      id,
+  const roles: Prisma.DiscordRoleCreateInput[] = []
+  for (const name of Object.values(ACCESS_LEVELS)) {
+    roles.push({
+      id: faker.random.numeric(18),
       role: {
         create: {
           accessLevel: {
@@ -33,8 +32,9 @@ function createFakeRoles(): Prisma.DiscordRoleCreateInput[] {
           },
         },
       },
-    }
-  })
+    })
+  }
+  return roles
 }
 
 async function getRandomFakeRole(): Promise<DiscordRole> {
@@ -64,26 +64,69 @@ const CHANNELS = [
   'ui-help',
 ]
 
+const TAGS = [
+  'android',
+  'auth',
+  'cli',
+  'cloudformation',
+  'containers',
+  'datastore',
+  'flutter',
+  'geo',
+  'graphql',
+  'ios',
+  'js',
+  'lambda',
+]
+
+function getRandomValue(arr: string[]): string {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
 function getRandomChannel(): string {
-  return CHANNELS[Math.floor(Math.random() * CHANNELS.length)]
+  return getRandomValue(CHANNELS)
+}
+function getRandomTag(): string {
+  return getRandomValue(TAGS)
+}
+
+function shouldAdd(odds = 0.5): boolean {
+  return Math.random() < odds
 }
 
 /**
  * @TODO accept argument for list length?
  */
 function createFakeQuestions(
-  length = 100
+  length = 3000
 ): Promise<Prisma.QuestionCreateInput[]> {
   return Promise.all(
     Array.from({ length }).map(async (_, i) => {
+      const randomFakeUser = await getRandomFakeUser()
+      const randomFakeRole = await getRandomFakeRole()
+      const randomTag = getRandomTag()
       const input: Prisma.QuestionCreateInput = {
         threadId: `999770${faker.random.numeric(12)}`,
         ownerId: '143912968529117185',
         channelName: getRandomChannel(),
         title: faker.random.words(15),
-        isSolved: faker.datatype.boolean(),
+        isSolved: shouldAdd(0.9),
         url: 'https://discord.com/channels/976838371383083068/976838372205137982/999770893356122152',
         createdAt: faker.date.recent(100),
+        tags: shouldAdd(0.9)
+          ? {
+              connectOrCreate: [
+                {
+                  where: {
+                    name: randomTag,
+                  },
+                  create: {
+                    id: `999770${faker.random.numeric(12)}`,
+                    name: randomTag,
+                  },
+                },
+              ],
+            }
+          : undefined,
         guild: {
           connectOrCreate: {
             where: {
@@ -99,27 +142,27 @@ function createFakeQuestions(
             id: faker.datatype.uuid(),
             participant: {
               connect: {
-                id: (await getRandomFakeUser()).id,
+                id: randomFakeUser.id,
               },
             },
             participantRoles: {
               connect: {
-                id: (await getRandomFakeRole()).id,
+                id: randomFakeRole.id,
               },
             },
           },
         },
       }
 
-      if (faker.datatype.boolean()) {
+      if (shouldAdd(0.8)) {
         input.answer = {
           create: {
             content: faker.random.words(15),
-            selectedBy: (await getRandomFakeUser()).id,
+            selectedBy: randomFakeUser.id,
             createdAt: faker.date.recent(10, input.createdAt),
             owner: {
               connect: {
-                id: (await getRandomFakeUser()).id,
+                id: randomFakeUser.id,
               },
             },
             participation: {
@@ -137,26 +180,41 @@ function createFakeQuestions(
   )
 }
 
+type SeedOptions = {
+  /**
+   * @default 3000
+   */
+  questions: number
+}
+
 /**
  * general seed function for local database
  */
-export async function seed() {
+export async function seed(options?: SeedOptions) {
+  const amountOfFakeQuestions = options?.questions ?? 3000
   await init()
-  for (const fakeUser of createFakeUsers()) {
-    await prisma.discordUser.create({
-      data: fakeUser,
-    })
-  }
-  for (const fakeRole of createFakeRoles()) {
-    await prisma.discordRole.create({
-      data: fakeRole,
-    })
-  }
-  for (const fakeQuestion of await createFakeQuestions()) {
-    await prisma.question.create({
-      data: fakeQuestion,
-    })
-  }
+  task('create users', async () => {
+    return Promise.all(
+      createFakeUsers().map((user) => prisma.discordUser.create({ data: user }))
+    )
+  })
+  task('create roles', async () => {
+    return Promise.all(
+      createFakeRoles().map((role) => prisma.discordRole.create({ data: role }))
+    )
+  })
+  task('create questions', async ({ setTitle }) => {
+    let count = 0
+    for (const fakeQuestion of await createFakeQuestions(
+      amountOfFakeQuestions
+    )) {
+      await prisma.question.create({
+        data: fakeQuestion,
+      })
+      count++
+      setTitle(`create questions (${count}/${amountOfFakeQuestions})`)
+    }
+  })
 }
 
 try {
