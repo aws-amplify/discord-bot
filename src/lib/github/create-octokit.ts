f872f376f1777c882f917120f7e45d9480c9daa6
josefaidt <josef.aidt@gmail.com>
2023-04-04 09:55:45 -0700

    patch github auth error

--- src/lib/github/create-octokit.ts
+++ src/lib/github/create-octokit.ts
@@ -5,7 +5,7 @@ import { fetchGitHubToken } from './fetch-github-token'
  * Creates an Octokit instance
  */
 export async function createOctokit() {
-  const token = await fetchGitHubToken()
+  const token = (await fetchGitHubToken()).catch(console.error)
   return new Octokit({
     auth: `token ${token}`,
   })
