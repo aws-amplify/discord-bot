997827edfb6f76a25f4f81fd972a6d2c6448be4a
josefaidt <josef.aidt@gmail.com>
2023-10-15 11:46:46 -0700

    improve test handling, prepares for e2e runs

--- apps/discord-bot-frontend/tests/setup/svelte-kit-routes.ts
+++
@@ -1,6 +0,0 @@
-import { beforeAll } from 'vitest'
-import { installPolyfills } from '@sveltejs/kit/node/polyfills'
-
-beforeAll(() => {
-  installPolyfills() // we're in Node, so we need to polyfill `fetch` and `Request` etc
-})
